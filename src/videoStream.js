c632bbcbbc9803cf4f4e9ad97f5bafffaecd1a81
eporkg <oyvindsbk@gmail.com>
2016-02-19 19:39:40 +0100

    wkeofe

--- src/videoStream.js
+++ src/videoStream.js
@@ -1,9 +1,10 @@
 var Firebase = require('firebase');
-var ref = new Firebase("https://ariot2016.firebaseio.com/");
+var Process = require('process');
+var ref = new Firebase("https://ariot2016.firebaseio.com/", Process.env["FIREBASE_TOKEN"]);
 var arDrone = require('ar-drone');
 var client  = arDrone.createClient();
 var vsRef = ref.child("videostream");		 
-var videoStream = client.getVideoStream();
-videoStream.on('data', vsRef.push({
-			 stream: console.log			 
-		 }));
+var videoStream = client.getPngStream();
+videoStream.on('data', function(buffer) {
+ vsRef.push(buffer);
+});
