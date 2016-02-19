d2cc6aa0c1b4305775a9d912b374f609477a118b
sirar87 <sirars@gmail.com>
2016-02-19 16:31:25 +0100

    Push video stream to FireBase.

--- src/videoStream.js
+++ src/videoStream.js
@@ -1,4 +1,9 @@
+var Firebase = require('firebase');
+var ref = new Firebase("https://ariot2016.firebaseio.com/");
 var arDrone = require('ar-drone');
 var client  = arDrone.createClient();
+var vsRef = ref.child("videostream");		 
 var videoStream = client.getVideoStream();
-videoStream.on('data', console.log);
+videoStream.on('data', vsRef.push({
+			 stream: console.log			 
+		 }));
