a4fb1cfa4632343adf03acba95880626addfa97c
eporkg <oyvindsbk@gmail.com>
2016-02-20 01:02:34 +0100

    fixed camera

--- src/raspicam.js
+++ src/raspicam.js
@@ -1,28 +1,22 @@
-var cameraOptions = {
-    width       : 600,
-    height      : 338,
-    mode        : "timelapse",
-    awb         : 'cloud',
-    output      : 'images/picture.jpg',
-    q           : 50,
-    rot         : 270,
-    nopreview   : true,
-    timeout     : 1000,
-    timelapse   : 9999,
-    th          : "0:0:0"
-};
- 
 var RaspiCam = require("raspicam");
 
-var camera = new RaspiCam({ cameraOptions });
- 
-// var camera = new require("raspicam")(cameraOptions);
+var camera = new RaspiCam({
+	mode: "photo",
+	output: "./photo/image.jpg",
+	encoding: "jpg",
+	timeout: 0 // take the picture immediately
+});
+
+camera.on("started", function( err, timestamp ){
+	console.log("photo started at " + timestamp );
+});
+
+camera.on("read", function( err, timestamp, filename ){
+	console.log("photo image captured with filename: " + filename );
+});
+
+camera.on("exit", function( timestamp ){
+	console.log("photo child process has exited at " + timestamp );
+});
 
 camera.start();
-camera.stop();
-// var Firebase = require('firebase');
-// var ref = new Firebase("https://ariot2016.firebaseio.com/", Process.env["FIREBASE_TOKEN"]);
-// var vsRef = ref.child("pictures");
-// vsRef.push({
-// 			 picturePath: ""
-// 		 });
