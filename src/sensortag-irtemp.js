c4c5cc67c5dd6d57502c852da4d68f65d3a5a50a
sirar87 <sirars@gmail.com>
2016-02-20 05:09:12 +0100

    add magnemeter.

--- src/sensortag-irtemp.js
+++ src/sensortag-irtemp.js
@@ -16,11 +16,31 @@ SensorTag.discover(function (tag) {
 	}
 	
 	function enableSensors() {		
-     console.log('enableIRTemperatureSensor');
+     console.log('enable sensors');
      tag.enableIrTemperature(notifyTemp);
 	 tag.enableAccelerometer(notifyAccelerometer);
 	 tag.enableHumidity(notifyHumidty);
+	 tag.enableMagnetometer(notifyMagnetometer);
    }
+   
+   function notifyMagnetometer() {
+			tag.notifyMagnetometer(readMagnetometer);
+	}
+
+	function readMagnetometer() {
+		tag.on('magnetometerChange', function(x, y, z) {
+		 console.log('\tx = %d', x.toFixed(1));
+	     console.log('\ty = %d', y.toFixed(1));
+		 console.log('\tz = %d', z.toFixed(1));
+		 var usersRef = ref.child("magnetometer");
+		 usersRef.push({
+			 x: x.toFixed(1),
+			 y: y.toFixed(1),
+			 z: z.toFixed(1),
+			 dateTime: new Date().toString()
+		 });	 
+	   });
+	}
 
 	function notifyHumidty() {
 			tag.notifyHumidity(readHumidty);
@@ -29,8 +49,8 @@ SensorTag.discover(function (tag) {
 	function readHumidty() {
 		tag.on('humidityChange', function(temperature, humidity) {
 		 console.log('\tTemperature = %d deg. C', temperature.toFixed(1));
-	     console.log('\tyHumidty = %d', humidity.toFixed(1));
-		 var usersRef = ref.child("accelerometer");
+	     console.log('\tHumidty = %d', humidity.toFixed(1));
+		 var usersRef = ref.child("humidity");
 		 usersRef.push({
 			 x: temperature.toFixed(1),
 			 y: humidity.toFixed(1),
