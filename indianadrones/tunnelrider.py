1393c068384893c835807a426f069730efb770b5
Sondre Lefsaker <sondrele@stud.ntnu.no>
2016-02-20 11:58:55 +0100

    refactor: Cleanup in code and remove formatting warnings according to PEP 8

--- indianadrones/tunnelrider.py
+++ indianadrones/tunnelrider.py
@@ -3,58 +3,52 @@ import serial
 import re
 import time
 from threading import Thread
-import asyncio
-from websocket import create_connection
 
 firebase = firebase.FirebaseApplication('https://ariot2016.firebaseio.com', authentication=None)
 
 buffer = [1000, 1000, 1000, 1000, 1000, 1000]
 
+MOCK = True
+
+
 def background_thread():
-	while True:
-		time.sleep(1)
-		result = firebase.post('/distance', 'distance_data: { right: ' + str(buffer[0]) + ', left: ' + str(buffer[1]) + ', top: ' + str(buffer[2]) + ', bottom: ' + str(buffer[3]) + ', front: ' + str(buffer[4]) + ', bottom: ' + str(buffer[5]) + '}')
-		print(result)
+    while True:
+        time.sleep(1)
+        result = firebase.post('/distance', 'distance_data: { right: ' + str(buffer[0]) + ', left: ' + str(buffer[1]) + ', top: ' + str(buffer[2]) + ', bottom: ' + str(buffer[3]) + ', front: ' + str(buffer[4]) + ', bottom: ' + str(buffer[5]) + '}')
+        print(result)
+
 
 def matchRegex(line):
-	if(1):
-		return True
-	else:
-		return False
+    if(1):
+        return True
+    else:
+        return False
+
 
 def validateLine(line):
-	
-	line = line.rstrip('\r\n')
-	if(len(line)==0):
-		return False
-	elif(line[0]=='[' and line[len(line)-1]==']'):
-		print("valid")
-		return True
-		
+
+    line = line.rstrip('\r\n')
+    if len(line) is 0:
+        return False
+    elif line[0] == '[' and line[len(line)-1] == ']':
+        print("valid")
+        return True
+
+
 def shouldFly():
-	#1. if drone is too close to roof, it should move down
-	
+    print("flying")
+    # 1. if drone is too close to roof, it should move down
 
+if not MOCK:
+    ser = serial.Serial('/dev/ttyUSB0', 9600)
 
-#ws = create_connection('ws://10.59.10.2:5000/indianadrones')
-#websocket.send(buffer)
-#print("websocket ok");
-		
-		
-ser = serial.Serial('/dev/ttyUSB0', 9600)
+    thread = Thread(target=background_thread)
+    thread.daemon = True
+    thread.start()
 
-thread = Thread(target=background_thread)
-thread.daemon = True
-thread.start()
-
-while 1:
-	line = ser.readline()
-	line = line.decode('unicode_escape', errors='ignore')
-	if(validateLine(line)):
-		line = re.search("(\[[0-9, ]+\])", line).group()
-		buffer = eval(line)
-		#result = firebase.post('/distance', 'distance_data: { right: ' + str(buffer[0]) + ', left: ' + str(buffer[1]) + ', top: ' + str(buffer[2]) + ', bottom: ' + str(buffer[3]) + ', front: ' + str(buffer[4]) + ', bottom: ' + str(buffer[5]) + '}')
-		
-
-
-
+    while True:
+        line = ser.readline()
+        line = line.decode('unicode_escape', errors='ignore')
+        if(validateLine(line)):
+            line = re.search("(\[[0-9, ]+\])", line).group()
+            buffer = eval(line)
