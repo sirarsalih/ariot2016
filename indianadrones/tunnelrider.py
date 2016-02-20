cb2ab57fa17bb8fca1c6514b33f3bd7398ce9a84
Sondre Lefsaker <sondrele@stud.ntnu.no>
2016-02-20 12:15:14 +0100

    Implement should_fly method

--- indianadrones/tunnelrider.py
+++ indianadrones/tunnelrider.py
@@ -36,8 +36,28 @@ def validateLine(line):
 
 
 def shouldFly():
-    print("flying")
-    # 1. if drone is too close to roof, it should move down
+    x, y, z = 0
+    # 1. if drone is too close to roof, it should move down.
+    # Then no if distance is over 12 cm no need.
+    if buffer[2] > 0 and buffer[2] < 12:
+        y = -1
+    else:
+        y = 0
+    # 2. Under 12 cm from right wall, fly away from it. Over 50 cm,
+    if buffer[0] > 0 and buffer[0] < 25:
+        x = -1
+    elif buffer[0] > 100 and buffer[0] < 1000:
+        x = 1
+    else:
+        x = 0
+    # Front is facing obstacle below 40 cm. GO BACK!
+    if buffer[4] > 0 and buffer[4] < 40:
+        z = -1
+    else:
+        z = 1
+
+    return (x, y, z)
+
 
 if not MOCK:
     ser = serial.Serial('/dev/ttyUSB0', 9600)
