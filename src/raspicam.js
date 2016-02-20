76582d07f60331e6b2345c64cffbb3f5c0cae183
sirar87 <sirars@gmail.com>
2016-02-20 01:46:15 +0100

    Add interval to push to firebase.

--- src/raspicam.js
+++ src/raspicam.js
@@ -10,36 +10,40 @@ var camera = new RaspiCam({
 	timeout: 0 // take the picture immediately
 });
 
-camera.on("started", function( err, timestamp ){
-	console.log("photo started at " + timestamp );
-});
+setInterval(function(){ 
+       camera.on("started", function( err, timestamp ){
+		console.log("photo started at " + timestamp );
+	});
+	
+	camera.on("read", function( err, timestamp, filename ){
+		console.log("photo image captured with filename: " + filename );
+	});
+	
+	camera.on("exit", function( timestamp ){
+		console.log("photo child process has exited at " + timestamp );
+	});
+	
+	camera.start();
+	
+	function base64_encode(file) {
+		var bitmap = fs.readFileSync(file);
+		return new Buffer(bitmap).toString('base64');
+	}
+	
+	function base64_decode(base64str, file) {
+		var bitmap = new Buffer(base64str, 'base64');
+		fs.writeFileSync(file, bitmap);
+		console.log('******** File created from base64 encoded string ********');
+	}
+	
+	var base64str = base64_encode('photo/image.jpg');
+	//base64_decode(base64str, 'copy.jpg');
+	
+	var vsRef = ref.child("pictures");
+	vsRef.push({
+				dateTime: new Date().toString(),
+				base64: base64str
+			});
+			console.log("Pushed base64 to FireBase...")
+}, 1000);
 
-camera.on("read", function( err, timestamp, filename ){
-	console.log("photo image captured with filename: " + filename );
-});
-
-camera.on("exit", function( timestamp ){
-	console.log("photo child process has exited at " + timestamp );
-});
-
-camera.start();
-
-function base64_encode(file) {
-    var bitmap = fs.readFileSync(file);
-    return new Buffer(bitmap).toString('base64');
-}
-
-function base64_decode(base64str, file) {
-    var bitmap = new Buffer(base64str, 'base64');
-    fs.writeFileSync(file, bitmap);
-    console.log('******** File created from base64 encoded string ********');
-}
-
-var base64str = base64_encode('photo/image.jpg');
-//base64_decode(base64str, 'copy.jpg');
-
-var vsRef = ref.child("pictures");
-vsRef.push({
-	         dateTime: new Date().toString(),
-			 base64: base64str
-		 });
